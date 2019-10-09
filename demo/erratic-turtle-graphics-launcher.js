59f2b344c3a6f714ecf7fbe47ea94c3ea4506113
Chris Pressey <cpressey@catseye.tc>
2019-10-09 03:49:34 +0100

    Show/allow editing of "Logo" program in a textarea.

--- demo/erratic-turtle-graphics-launcher.js
+++ demo/erratic-turtle-graphics-launcher.js
@@ -4,7 +4,7 @@
  */
 
 function launch(config) {
-  var div=DAM.maker('div'), button=DAM.maker('button'), canvas=DAM.maker('canvas');
+  var div=DAM.maker('div'), button=DAM.maker('button'), textarea=DAM.maker('textarea'), canvas=DAM.maker('canvas');
 
   var can = canvas({ width: 1000, height: 400 });
   config.container.appendChild(can);
@@ -24,6 +24,10 @@ function launch(config) {
       value: "seterr 0 0 setxyp 0.125 0.5 lt 90 repeat 7 [ repeat 4500 [ fd 1.0 lt 4 ] shiftxyp 0.125 0.0 shifterr 0.00375 0.05 ]"
     },
     {
+      text: 'Stars',
+      value: "seterr 0 0 setxyp 0.125 0.666 lt 90 lt 18 repeat 7 [ repeat 500 [ fd 150 lt 144 ] shiftxyp 0.125 0.0 shifterr 0.00375 0.3 ]"
+    },
+    {
       text: 'Circle Chain',
       value: "seterr 0 0 setxyp 1.0 0.5 lt 90 repeat 7 [ repeat 1845 [ fd 2.0 lt 4 ] lt 180 shifterr 0.00375 0.225 ] repeat 7 [ repeat 1845 [ fd 2.0 lt 4 ] lt 180 shifterr -0.00375 -0.225 ]"
     }
@@ -38,8 +42,7 @@ function launch(config) {
     interpretInstrs(i, turtle);
   }
 
-  run();
-
+  var programTextarea = textarea({ rows: 4, cols: 80 }, program);
   var controlPanel = div(
     div(
       DAM.makeSelect({
@@ -47,13 +50,24 @@ function launch(config) {
         options: options,
         onchange: function(option) {
           program = option.value;
+          programTextarea.innerHTML = program;
           run();
         }
       })
     ),
     div(
-      button("Re-roll", { onclick: function() { run(); }})
+      programTextarea,
+    ),
+    div(
+      button("Run", {
+        onclick: function() {
+          program = programTextarea.value;
+          program = run();
+        }
+      })
     )
   );
   config.container.appendChild(controlPanel);
+
+  run();
 }
